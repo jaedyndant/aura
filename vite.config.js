1bb5ded18e0a8cce9667186b77f4165c4662aad0
jaedyndant <jaedyndant@users.noreply.github.com>
2026-06-24 17:46:16 +0000

    fix: auto-detect Vercel env for base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aura/',
+  base: process.env.VERCEL ? '/' : '/aura/',
   build: {
     outDir: 'dist',
     assetsInlineLimit: 0,
